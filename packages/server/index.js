df238538dfdcfb1b8c83d58863305a9b77c3920d
Jimmy Sorza <jimmysorza@gmail.com>
2019-12-12 16:11:12 -0600

    add message log

--- packages/server/index.js
+++ packages/server/index.js
@@ -9,4 +9,8 @@ server.listen(PORT, () => console.log(`Listen on *: ${PORT}`));
 io.on("connection", socket => {
   const { id } = socket.client;
   console.log(`User connected: ${id}`);
+  socket.on("chat message", msg => {
+    console.log(`${id}: ${msg}`);
+  });
 });
+
