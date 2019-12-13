a0b2038f6f7a46c470ce72b3ee5f7124cc7b80c6
Jimmy Sorza <jimmysorza@gmail.com>
2019-12-12 18:12:05 -0600

    use material-ui for responsive

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,9 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import io from "socket.io-client";
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Paper,
+  Grid,
+  TextField,
+  FormControl,
+  Button
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(1),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 
 const socket = io.connect("http://localhost:5000");
 
 function App() {
+  const classes = useStyles();
 
   const [msg, setMsg] = useState('');
   const [chat, setChat] = useState([]);
@@ -43,21 +73,50 @@ function App() {
 
 
   return (
-    <div>
-      <span>Nickname</span>
-      <input
-        name="nickname"
-        onChange={e => onTextChangeNick(e)}
-        value={nickname}
-      />
-      <span>Message</span>
-      <input
-        name="msg"
-        onChange={e => onTextChangeMsg(e)}
-        value={msg}
-      />
-      <button onClick={onMessageSubmit}>Send</button>
-      <div>{renderChat()}</div>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            web chat with socket.io and react
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={3}>
+        <Grid item xs={12} >
+          <Paper className={classes.paper}>
+              <TextField
+                id="nickname"
+                label="Nickname"
+                name="nickname"
+                onChange={e => onTextChangeNick(e)}
+                value={nickname}
+              />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper>
+            {renderChat()}
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <FormControl fullWidth variant="outlined">
+              <TextField
+                id="msg"
+                label="Mensaje"
+                name="msg"
+                onChange={e => onTextChangeMsg(e)}
+                value={msg}
+              />
+            </FormControl>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Button variant="contained" color="primary" onClick={onMessageSubmit}>
+            Enviar
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
